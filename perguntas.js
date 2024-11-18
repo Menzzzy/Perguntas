55f21ec1501366029e64358765804314fb6dd156
Menzzzy <gabrieltobias20@outlook.com>
2024-11-18 19:04:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curiosidades',
+    'Qual o nome do pequeno passáro vermelho dos Angry Birds?',
+    'Red'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Qual o número de pi?',
+    '3,14 é o que usamos, porém à infinitas casas decimais'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Quantos te armazenamento o cérebro possui?',
+    'Em média 1 Milhão de "gigabytes" de armazenamento'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Quando a Apollo 11 foi lançada?',
+    'Foi em 1969'
+)
